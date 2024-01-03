153d686396aad5e57fd2452d4550f72a3f023ccb
B1tWasTaken <71984191+B1tWasTaken@users.noreply.github.com>
2024-01-03 15:02:24 -0600

    Update flipHandler.ts

--- src/flipHandler.ts
+++ src/flipHandler.ts
@@ -22,12 +22,8 @@ export async function flipHandler(bot: MyBot, flip: Flip) {
         }
     }, 10000)
     let isBed = flip.purchaseAt.getTime() > new Date().getTime()
-    let delayUntilBuyStart = isBed ? flip.purchaseAt.getTime() - new Date().getTime() : getConfigProperty('FLIP_ACTION_DELAY')
-
     bot.lastViewAuctionCommandForPurchase = `/viewauction ${flip.id}`
-    await sleep(delayUntilBuyStart)
     bot.chat(bot.lastViewAuctionCommandForPurchase)
-
     printMcChatToConsole(
         `§f[§4BAF§f]: §fTrying to purchase flip${isBed ? ' (Bed)' : ''}: ${flip.itemName} §for ${numberWithThousandsSeparators(
             flip.startingBid
@@ -51,8 +47,8 @@ export async function flipHandler(bot: MyBot, flip: Flip) {
               return }
           if (!(title.toString().includes('Confirm Purchase')) || (title.toString().includes('BIN Auction View'))) {
              bot.removeAllListeners('windowOpen')
-              bot.state = null
-              return
+             bot.state = null
+             return 
          }    
         await sleep(getConfigProperty('CLICK_DELAY');
     }
