c4f73074089c8bc9ce267be902e68b9362162214
B1tWasTaken <71984191+B1tWasTaken@users.noreply.github.com>
2024-01-06 17:24:39 -0600

    Update flipHandler.ts

--- src/flipHandler.ts
+++ src/flipHandler.ts
@@ -5,52 +5,78 @@ import { log, printMcChatToConsole } from './logger';
 import { clickWindow, getWindowTitle, numberWithThousandsSeparators, sleep } from './utils';
 
 export async function flipHandler(bot: MyBot, flip: Flip) {
-    flip.purchaseAt = new Date(flip.purchaseAt);
+    flip.purchaseAt = new Date(flip.purchaseAt)
 
     if (bot.state) {
         setTimeout(() => {
-            flipHandler(bot, flip);
-        }, 1100);
-        return;
+            flipHandler(bot, flip)
+        }, 1100)
+        return
     }
-    bot.state = 'purchasing';
+
+    bot.state = 'purchasing'
+
     let timeout = setTimeout(() => {
         if (bot.state === 'purchasing') {
-            log("Resetting 'bot.state === purchasing' lock");
-            bot.state = null;
-            bot.removeAllListeners('windowOpen');
+            log("Resetting 'bot.state === purchasing' lock")
+            bot.state = null
+            bot.removeAllListeners('windowOpen')
         }
-    }, 10000);
-    let isBed = flip.purchaseAt.getTime() > new Date().getTime();
-    bot.lastViewAuctionCommandForPurchase = `/viewauction ${flip.id}`;
-    bot.chat(bot.lastViewAuctionCommandForPurchase);
+    }, 10000)
+
+    let isBed = flip.purchaseAt.getTime() > new Date().getTime()
+    bot.lastViewAuctionCommandForPurchase = `/viewauction ${flip.id}`
+    bot.chat(bot.lastViewAuctionCommandForPurchase)
     printMcChatToConsole(
         `§f[§4BAF§f]: §fTrying to purchase flip${isBed ? ' (Bed)' : ''}: ${flip.itemName} §for ${numberWithThousandsSeparators(
             flip.startingBid
         )} coins (Target: ${numberWithThousandsSeparators(flip.target)})`
-    );
+    )
+
     if (getConfigProperty('USE_WINDOW_SKIPS')) {
-        useWindowSkipPurchase(flip, isBed);
+        useWindowSkipPurchase(flip, isBed)
         setTimeout(() => {
-            bot.state = null;
-            clearTimeout(timeout);
-        }, 2500);
+            bot.state = null
+            clearTimeout(timeout)
+        }, 2500)
     }
-  if (isBed) {
-    let coflBuyDelay = new Date().getTime() - flip.purchaseAt.getTime()
-    let possibleClicks = Math.ceil(coflBuyDelay / 200) + 1
-    printMcChatToConsole('DEBUG: Attempting to spam the Bed and clicking: ${possibleClicks}')
+
+    if (isBed) {
+        let coflBuyDelay = new Date().getTime() - flip.purchaseAt.getTime()
+        let possibleClicks = Math.ceil(coflBuyDelay / 200) + 1
+        printMcChatToConsole(`DEBUG: Attempting to spam the Bed and clicking: ${possibleClicks}`)
+        bot.addListener('windowOpen', async (window) => {
+            let title = getWindowTitle(window)
+            if (title.toString().includes('BIN Auction View')) {
+                printMcChatToConsole('DEBUG: Clicking Bed (srry if this spams lmfao)')
+                for (let x = 0; x < possibleClicks; x++) {
+                    clickWindow(bot, 31)
+                    await sleep(200)
+                    title = getWindowTitle(window)
+                }
+            }
+            for (let y = 0; y < 3; y++) {
+                if (title.toString().includes('Confirm Purchase')) {
+                    await sleep(getConfigProperty('FLIP_ACTION_DELAY'))
+                    clickWindow(bot, 11)
+                    bot.removeAllListeners('windowOpen')
+                    bot.state = null
+                    return
+                }
+            }
+        })
+    } else {
+        useRegularPurchase(bot)
+    }
+}
+
+async function useRegularPurchase(bot: MyBot) {
     bot.addListener('windowOpen', async (window) => {
         let title = getWindowTitle(window)
         if (title.toString().includes('BIN Auction View')) {
-            printMcChatToConsole('DEBUG: Clicking Bed (srry if this spams lmfao)')
-            for (let x = 0; x = possibleClicks; x++) {
-            await sleep(200)
+            await sleep(getConfigProperty('FLIP_ACTION_DELAY'))
             clickWindow(bot, 31)
-            title = getWindowTitle(window)
-            }
         }
-        for (let y = 0; y = 3, y++) {
         if (title.toString().includes('Confirm Purchase')) {
             await sleep(getConfigProperty('FLIP_ACTION_DELAY'))
             clickWindow(bot, 11)
@@ -58,30 +84,10 @@ export async function flipHandler(bot: MyBot, flip: Flip) {
             bot.state = null
             return
         }
-        }
-    });
-}    else {
-        useRegularPurchase(bot)
-}
-
-async function useRegularPurchase(bot: MyBot) {
-    bot.addListener('windowOpen', async (window) => {
-        let title = getWindowTitle(window);
-        if (title.toString().includes('BIN Auction View')) {
-            await sleep(getConfigProperty('FLIP_ACTION_DELAY'));
-            clickWindow(bot, 31);
-        }
-        if (title.toString().includes('Confirm Purchase')) {
-            await sleep(getConfigProperty('FLIP_ACTION_DELAY'));
-            clickWindow(bot, 11);
-            bot.removeAllListeners('windowOpen');
-            bot.state = null;
-            return;
-        }
-    });
+    })
 }
 
 async function useWindowSkipPurchase(flip: Flip, isBed: boolean) {
     // bans you so removing it alltogether :p
 }
-
+8
